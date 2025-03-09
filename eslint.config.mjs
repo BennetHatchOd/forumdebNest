0016b487d1712c72941bb77963e0ae742ad49b8f
Vladimir NG <potd3@ukr.net>
2025-03-09 13:35:05 +0200

    eclint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,43 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+//import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+//   {languageOptions: { globals: globals.browser }},
+  // pluginJs.configs.recommended,
+  // ...tseslint.configs.recommended,
+    {
+      ignores: ['dist', 'node_modules'], // Игнорируем папки
+    },
+    {
+      languageOptions: {
+        parser: tsparser,
+        parserOptions: {
+          project: './tsconfig.json',
+          sourceType: 'module',
+        },
       },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+      plugins: {
+        '@typescript-eslint': tseslint,
+        prettier: prettierPlugin,
+      },
+      rules: {
+        'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'warn',
+        'no-debugger': 'error',
+        "indent": ["error", 4], // Требуем 4 пробела
+        "prettier/prettier": ["error", { tabWidth: 4 }],  
+        "no-multiple-empty-lines": "off", // Игнорирует пустые строки   
+        "indent": "off", // Игнорирует отступы и табуляцию  
       },
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+];
