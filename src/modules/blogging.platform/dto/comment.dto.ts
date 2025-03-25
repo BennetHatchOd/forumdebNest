23e8fc8dad6519172cbe7174bbe2c69c7e8dc036
Vladimir NG <potd3@ukr.net>
2025-03-25 16:06:58 +0200

    add post and likes dto

---
+++ src/modules/blogging.platform/dto/comment.dto.ts
@@ -0,0 +1,10 @@
+
+// export class CommentType {
+//     constructor(
+//         public parentPostId: string,
+//         public content: string,
+//         public commentatorInfo: CommentatorInfoType,
+//         public createdAt: Date,
+//        // public likesInfo: LikesRecipientType,
+//     ) {}
+// }
