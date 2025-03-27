2d2aef72fd3c20c18e4799d95096f3de7983457c
Vladimir NG <potd3@ukr.net>
2025-03-27 17:48:12 +0200

    add modules import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { BloggingPlatformModule } from './modules/blogging.platform/blogging.platform.module';
 import { DB_NAME, mongoURI } from './core/setting';
+import { UserSystemModule } from './modules/users-system/user.system.module';
+import { TestingModule } from './modules/testing/testing.module';
 
 @Module({
     imports: [
         MongooseModule.forRoot(mongoURI, { dbName: DB_NAME }),
         BloggingPlatformModule,
+        UserSystemModule,
+        TestingModule,
     ],
     controllers: [AppController],
     providers: [AppService],
