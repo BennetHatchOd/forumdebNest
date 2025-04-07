b593e8a74e207bb7e3899477b17d6f40134e2345
Vladimir NG <potd3@ukr.net>
2025-04-07 10:31:49 +0300

    add AuthGuard in user endpoins

--- src/modules/users-system/api/user.controller.ts
+++ src/modules/users-system/api/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { URL_PATH } from 'src/core/setting';
 import { UserInputDto } from '../dto/input/user.input.dto';
 import { UserViewDto } from '../dto/view/user.view.dto';
@@ -6,7 +6,9 @@ import { UserService } from '../apllication/user.service';
 import { UserQueryRepository } from '../infrastucture/query/user.query.repository';
 import { PaginatedViewDto } from '../../../core/dto/base.paginated.view.dto';
 import { GetUserQueryParams } from '../dto/input/get.user.query.params.input.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('basic'))
 @Controller(URL_PATH.users)
 export class UserControllers {
     constructor(
@@ -17,8 +19,6 @@ export class UserControllers {
     @Post()
     @HttpCode(HttpStatus.CREATED)
     async createUser(@Body() user: UserInputDto):Promise<UserViewDto> {
-    // 
-    // Create new blog
 
         const createId: string = await this.userService.create(user);
         const userView: UserViewDto = await this.userQueryRepository.findById(createId);
@@ -38,9 +38,7 @@ export class UserControllers {
 
     @Delete(':id')
     @HttpCode(HttpStatus.NO_CONTENT)
-    async deleteBlog(@Param('id') id: string): Promise<void>{
-    //
-    // Delete blog specified by id
+    async deleteUser(@Param('id') id: string): Promise<void>{
 
         return await this.userService.delete(id)
     }
