48140b6461f80fb9e42e33f4bccbc8979386a3e7
Vladimir NG <potd3@ukr.net>
2025-04-09 06:23:48 +0300

    add comments

--- src/modules/users-system/infrastucture/auth.repository.ts
+++ src/modules/users-system/infrastucture/auth.repository.ts
@@ -13,6 +13,9 @@ export class AuthRepository {
     async getPartUserByLoginEmail(loginOrEmail: string): Promise<{id:string, passHash:string}|null> {
 
         const checkedUser: UserDocument|null = await this.UserModel.findOne(
+            // returns information about the user who has
+            // a login or email that matches the passed value
+
             {$and:[
                 {isConfirmEmail: true},
                 {$or: [
@@ -27,6 +30,8 @@ export class AuthRepository {
     }
 
     async checkUniq(loginCheck: string, emailCheck: string):Promise<string[]|null>  {
+        // checks the uniqueness of the entered login and email, in case of duplication,
+        // returns an array indicating the duplicated field
 
         const existEmail = await this.UserModel.countDocuments({ email: emailCheck })
         const existLogin = await this.UserModel.countDocuments({ login: loginCheck })
@@ -47,6 +52,7 @@ export class AuthRepository {
     }
 
     async findByConfirmCode(code: string): Promise<UserDocument|null> {
+        // search user with not verifed email
 
         const searchItem: UserDocument | null
             = await this.UserModel.findOne({
@@ -55,15 +61,19 @@ export class AuthRepository {
         return searchItem;
     }
 
-    async foundUserWithOutEmail(mail: string):Promise <UserDocument|null>{
+    async foundUserWithOutEmail(email: string):Promise <UserDocument|null>{
+        // search user with not verifed email
+
         const searchItem: UserDocument | null
-            = await this.UserModel.findOne({email: mail,
+            = await this.UserModel.findOne({email: email,
                                             isConfirmEmail: false})
 
         return searchItem;
     }
 
     async foundUserIdByEmail(mail: string):Promise <string|null>{
+        // search user with verifed email
+
         const searchItem: UserDocument | null
             = await this.UserModel.findOne({email: mail,
                                             isConfirmEmail: true})
