136e49b249657f9c8c071ce52b2a7d48a2b57dc0
Vladimir NG <potd3@ukr.net>
2025-04-16 20:08:30 +0300

    move url path

---
+++ src/core/url.path.setting.ts
@@ -0,0 +1,23 @@
+export const GLOBAL_PREFIX = 'api';
+export const URL_PATH = {
+    base: '/',
+    blogs: '/blogs',
+    posts: '/posts',
+    users: '/users',
+    auth: '/auth',
+    devices: '/security/devices',
+    comments: '/comments',
+    testing: '/testing/all-data',
+};
+
+export const AUTH_PATH = {
+    login: '/login',
+    //     logout:          '/logout',
+    registration: '/registration',
+    confirmation: '/registration-confirmation',
+    resentEmail: '/registration-email-resending',
+    askNewPassword: '/password-recovery',
+    confirmNewPassword: '/new-password',
+    //     refresh:         '/refresh-token',
+    aboutMe: '/me',
+};
