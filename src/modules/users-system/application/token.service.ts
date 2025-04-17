2d6af49329b45348c18d901c846b2e49a69d2540
Vladimir NG <potd3@ukr.net>
2025-04-17 20:54:19 +0300

    update tokenService

--- src/modules/users-system/application/token.service.ts
+++ src/modules/users-system/application/token.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as process from 'node:process';
+import { UserConfig } from '../config/user.config';
 
 @Injectable()
 export class TokenService {
-    constructor(private readonly jwtService: JwtService,){ }
+    constructor(private readonly jwtService: JwtService,
+                private userConfig: UserConfig,
+    ){ }
 
     async createAccessToken(id: string): Promise<string>{
         return this.jwtService.signAsync(
-            {sub: id},
-            {secret: process.env.ACCESS_TOKEN_SECRET_KEY,
-            expiresIn: process.env.TIME_LIFE_ACCESS_TOKEN,});
+            {userId: id},
+            {secret: this.userConfig.refreshTokenSecret,
+            expiresIn: this.userConfig.timeLifeAccessToken});
     }
 
     async createRefreshToken(id: string): Promise<string>{
         return this.jwtService.signAsync(
-            {sub: id},
-            {secret: process.env.REFRESH_TOKEN_SECRET_KEY,
-             expiresIn: process.env.TIME_LIFE_REFRESH_TOKEN,});
+            {userId: id},
+            {secret: this.userConfig.refreshTokenSecret,
+             expiresIn: this.userConfig.timeLifeRefreshToken});
     }
 
     async verifyAccessToken(token: string): Promise<string>{
         const payload = await this.jwtService.verifyAsync(token,
-            { secret: process.env.ACCESS_TOKEN_SECRET_KEY });
-        return payload.sub;
+            { secret: this.userConfig.accessTokenSecret });
+        return payload.userId;
     }
 
     async verifyRefreshToken(token: string): Promise<string>{
         const payload = await this.jwtService.verifyAsync(token,
-            { secret: process.env.REFRESH_TOKEN_SECRET_KEY });
-        return payload.sub;
+            { secret: this.userConfig.refreshTokenSecret });
+        return payload.userId;
     }
 }
