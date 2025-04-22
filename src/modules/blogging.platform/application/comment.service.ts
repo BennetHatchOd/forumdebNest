0d5253fce0a3187d1f96abc20206ea3a5b261029
Vladimir NG <potd3@ukr.net>
2025-04-22 18:20:53 +0300

    add create Comment

--- src/modules/blogging.platform/application/comment.service.ts
+++ src/modules/blogging.platform/application/comment.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CommentRepository } from '../infrastucture/comment.repository';
+import { CommentViewDto } from '../dto/view/comment.view.dto';
+import { CommentInputDto } from '../dto/input/comment.input.dto';
+import { Comment, CommentDocument } from '../domain/comment.entity';
+import { UserQueryExternalRepository } from '../../users-system/infrastucture/query/user.query.external.repository';
+import { CreateCommentDto } from '../dto/create.comment.dto';
 
 @Injectable()
 export class CommentService {
     constructor(
         private commentRepository: CommentRepository,
+        private userQueryExternalRepository: UserQueryExternalRepository,
     ) {}
 
+    async create(postId: string, comment: CommentInputDto, userId: string): Promise<string> {
+
+        const commentatorInfo = await this.userQueryExternalRepository.findNameById(userId)
+        const createCommentDto: CreateCommentDto = CreateCommentDto.createInstance(postId, comment, commentatorInfo!);
+        const newComment: CommentDocument = Comment.createInstance(createCommentDto);
+        await this.commentRepository.save(newComment);
+        return newComment._id.toString();
+    }
 }
