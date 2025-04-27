e066f9b2bfd71be7e6bc855be570157810590836
Vladimir NG <potd3@ukr.net>
2025-04-27 16:14:59 +0300

    add factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { configModule } from './setup/config.module';
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
@@ -8,12 +8,19 @@ import { UserSystemModule } from './modules/users-system/user.system.module';
 import { TestingModule } from './modules/testing/testing.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { CoreModule } from './core/core.module';
-import { DB_NAME, mongoURI } from './core/setting';
+import { CoreConfig } from './core/core.config';
 
 @Module({
     imports: [
         configModule,
-        MongooseModule.forRoot(mongoURI, { dbName: DB_NAME }),
+        MongooseModule.forRootAsync({
+            useFactory: (coreConfig:CoreConfig) =>{
+                const  url = new URL(coreConfig.mongoURI)
+                url.pathname = '/' + coreConfig.dbName
+                return { uri: url.toString()}
+            },
+            inject:[CoreConfig],
+         }),
         BloggingPlatformModule,
         UserSystemModule,
         TestingModule,
@@ -23,4 +30,16 @@ import { DB_NAME, mongoURI } from './core/setting';
     controllers: [AppController, ],
     providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+    // static async forRoot(coreConfig: CoreConfig): Promise<DynamicModule> {
+    //     // такой мудрёный способ мы используем, чтобы добавить к основным модулям необязательный модуль.
+    //     // чтобы не обращаться в декораторе к переменной окружения через process.env в декораторе, потому что
+    //     // запуск декораторов происходит на этапе склейки всех модулей до старта жизненного цикла самого NestJS
+    //
+    //     return {
+    //         module: AppModule,
+    //         imports: [...(coreConfig.includeTestingModule ? [TestingModule] : [])], // Add dynamic modules here
+    //     };
+    // }
+}
