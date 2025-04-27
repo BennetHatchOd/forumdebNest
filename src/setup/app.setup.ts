3f5c171e35bba192f863b969cc793468ded9c860
Vladimir NG <potd3@ukr.net>
2025-04-27 16:09:38 +0300

    fix global prefix

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -1,13 +1,12 @@
 import { INestApplication } from '@nestjs/common';
-import { globalPrefixSetup } from './global-prefix.setup';
-import { swaggerSetup } from './swagger.setup';
-import { HttpExceptionFilter } from '@core/exceptions/filters/http.exception.filter';
 import { pipesSetup } from './pipes.setup';
+import { swaggerSetup } from '@src/setup/swagger.setup';
+import { HttpExceptionFilter } from '@core/exceptions/filters/http.exception.filter';
 
-export function appSetup(app: INestApplication, isSwaggerEnable: boolean) {
+export function appSetup(app: INestApplication, isSwaggerEnable: boolean, globalPrefix: string) {
     pipesSetup(app);
-    globalPrefixSetup(app);
-    swaggerSetup(app, isSwaggerEnable);
+    app.setGlobalPrefix(globalPrefix);
+    swaggerSetup(app, isSwaggerEnable, globalPrefix);
 
-    //app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalFilters(new HttpExceptionFilter());
 }
--- src/setup/global-prefix.setup.ts
+++
@@ -1,7 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-import { GLOBAL_PREFIX } from '../core/url.path.setting';
-
-export function globalPrefixSetup(app: INestApplication) {
-    //специальный метод, который добавляет ко всем маршрутам /GLOBAL_PREFIX
-    app.setGlobalPrefix(GLOBAL_PREFIX);
-}
