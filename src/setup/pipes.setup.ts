2f9f56ac9e5641a520bfa64795b6e2105e60dfba
Vladimir NG <potd3@ukr.net>
2025-04-27 16:17:38 +0300

    add validate transformating

--- src/setup/pipes.setup.ts
+++ src/setup/pipes.setup.ts
@@ -1,47 +1,48 @@
-import { INestApplication, ValidationError, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, HttpException, INestApplication, ValidationError, ValidationPipe } from '@nestjs/common';
+import { DomainException, Extension } from '@core/exceptions/domain.exception';
+import { DomainExceptionCode } from '@core/exceptions/domain.exception.code';
 
-// export const errorFormatter = (
-//     errors: ValidationError[],
-//     errorMessage?: any,
-// ): Extension[] => {
-//     const errorsForResponse = errorMessage || [];
-//
-//     for (const error of errors) {
-//         if (!error.constraints && error.children?.length) {
-//             errorFormatter(error.children, errorsForResponse);
-//         } else if (error.constraints) {
-//             const constrainKeys = Object.keys(error.constraints);
-//
-//             for (const key of constrainKeys) {
-//                 errorsForResponse.push({
-//                     message: error.constraints[key]
-//                         ? `${error.constraints[key]}; Received value: ${error?.value}`
-//                         : '',
-//                     key: error.property,
-//                 });
-//             }
-//         }
-//     }
-//
-//     return errorsForResponse;
-// };
+export const errorFormatter = (
+    errors: ValidationError[],
+    errorMessage?: any,
+): Extension[] => {
+    const errorsForResponse = errorMessage || [];
+
+    for (const error of errors) {
+        if (!error.constraints && error.children?.length) {
+            errorFormatter(error.children, errorsForResponse);
+        } else if (error.constraints) {
+            const constrainKeys = Object.keys(error.constraints);
+
+            for (const key of constrainKeys) {
+                errorsForResponse.push({
+                    message: error.constraints[key]
+                        ? `${error.constraints[key]}; Received value: ${error?.value}`
+                        : '',
+                    field: error.property,
+                });
+            }
+        }
+    }
+
+    return errorsForResponse;
+};
 
 export function pipesSetup(app: INestApplication) {
     app.useGlobalPipes(
-        // new ObjectIdValidationTransformationPipe(),
         new ValidationPipe({
             transform: true,
             whitelist: true,
             stopAtFirstError: true,
-            // exceptionFactory: (errors) => {
-            //     const formattedErrors = errorFormatter(errors);
-            //
-            //     throw new DomainException({
-            //         code: DomainExceptionCode.ValidationError,
-            //         message: 'Validation failed',
-            //         extensions: formattedErrors,
-            //     });
-            // },
+            exceptionFactory: (errors) => {
+                const formattedErrors = errorFormatter(errors);
+
+                throw new DomainException({
+                    code: DomainExceptionCode.ValidationError,
+                    message: 'Validation failed',
+                    extension: formattedErrors,
+                });
+            },
         }),
     );
 }
