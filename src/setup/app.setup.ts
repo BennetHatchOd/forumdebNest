7d115fe201578b5f7bc58ae23a039fc0b0422f63
Vladimir NG <potd3@ukr.net>
2025-04-29 02:15:53 +0300

    include DomainExceptionFilter

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -2,11 +2,12 @@ import { INestApplication } from '@nestjs/common';
 import { pipesSetup } from './pipes.setup';
 import { swaggerSetup } from '@src/setup/swagger.setup';
 import { HttpExceptionFilter } from '@core/exceptions/filters/http.exception.filter';
+import { DomainExceptionFilter } from '@core/exceptions/filters/domain.exception.filter';
 
 export function appSetup(app: INestApplication, isSwaggerEnable: boolean, globalPrefix: string) {
     pipesSetup(app);
     app.setGlobalPrefix(globalPrefix);
     swaggerSetup(app, isSwaggerEnable, globalPrefix);
 
-    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalFilters(new DomainExceptionFilter(), new HttpExceptionFilter());
 }
