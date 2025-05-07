274c9e957e140db40a747fdb0c81e4414e232a94
Vladimir NG <potd3@ukr.net>
2025-05-07 15:55:30 +0300

    add validation

--- src/modules/users-system/dto/input/user.input.dto.ts
+++ src/modules/users-system/dto/input/user.input.dto.ts
@@ -1,8 +1,9 @@
-import { IsEmail, IsString, Length } from 'class-validator';
+import { IsEmail, IsString, Length, Matches } from 'class-validator';
 import { UserFieldRestrict } from '../../field.restrictions';
 
 export class UserInputDto {
     @IsString()
+    @Matches('^[a-zA-Z0-9_-]*$')
     @Length(UserFieldRestrict.loginMin, UserFieldRestrict.loginMax)
     login: string;
 
