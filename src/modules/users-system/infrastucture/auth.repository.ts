1188c115d7e1f25e9ab15f01f6240f2b00e6efa3
Vladimir NG <potd3@ukr.net>
2025-05-07 13:17:48 +0300

    move methods

--- src/modules/users-system/infrastucture/auth.repository.ts
+++ src/modules/users-system/infrastucture/auth.repository.ts
@@ -6,78 +6,13 @@ import { NewPassword, NewPasswordDocument, NewPasswordModelType } from '../domai
 @Injectable()
 export class AuthRepository {
 
-    constructor(@InjectModel(User.name) private UserModel: UserModelType,
+    constructor(//@InjectModel(User.name) private UserModel: UserModelType,
                 @InjectModel(NewPassword.name) private NewPasswordModel: NewPasswordModelType,
                 ){}
 
-    async getPartUserByLoginEmail(loginOrEmail: string): Promise<{id:string, passHash:string}|null> {
 
-        const checkedUser: UserDocument|null = await this.UserModel.findOne(
-            // returns information about the user who has
-            // a login or email that matches the passed value
-
-            {$and:[
-                {isConfirmEmail: true},
-                {$or: [
-                        {login: loginOrEmail},
-                        {email: loginOrEmail}]
-                }]
-            })
-
-        return checkedUser === null
-            ? null
-            : {id: checkedUser._id.toString(), passHash: checkedUser.passwordHash};
-    }
-
-    async checkUniq(loginCheck: string, emailCheck: string):Promise<string[]|null>  {
-        // checks the uniqueness of the entered login and email, in case of duplication,
-        // returns an array indicating the duplicated field
-
-        const existEmail = await this.UserModel.countDocuments({ email: emailCheck })
-        const existLogin = await this.UserModel.countDocuments({ login: loginCheck })
-
-        if(existEmail > 0 || existLogin > 0) {
-            const arrayErrors: Array<string> = []
-            if(existEmail > 0)
-                arrayErrors.push('mail')
-            if(existLogin > 0)
-                arrayErrors.push('login')
-            return arrayErrors;
-        }
-        return null;
-    }
-
-    async save(changedItem: UserDocument|NewPasswordDocument): Promise<void> {
+    async save(changedItem: NewPasswordDocument): Promise<void> {
         await changedItem.save();
-    }
-
-    async findByConfirmCode(code: string): Promise<UserDocument|null> {
-        // search user with not verifed email
-
-        const searchItem: UserDocument | null
-            = await this.UserModel.findOne({
-                                        'confirmEmail.code': code,
-                                        isConfirmEmail: false})
-        return searchItem;
-    }
-
-    async foundUserWithOutEmail(email: string):Promise <UserDocument|null>{
-        // search user with not verifed email
-
-        const searchItem: UserDocument | null
-            = await this.UserModel.findOne({email: email,
-                                            isConfirmEmail: false})
-
-        return searchItem;
-    }
-
-    async foundUserIdByEmail(mail: string):Promise <string|null>{
-        // search user with verifed email
-
-        const searchItem: UserDocument | null
-            = await this.UserModel.findOne({email: mail,
-                                            isConfirmEmail: true})
-        return searchItem ? searchItem._id.toString() : null;
     }
 
     async findPasswordRecovery(recoveryCode: string): Promise<NewPasswordDocument|null> {
--- src/modules/users-system/infrastucture/user.repository.ts
+++ src/modules/users-system/infrastucture/user.repository.ts
@@ -6,17 +6,83 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserRepository {
 
-    constructor(@InjectModel(User.name) private userModel: UserModelType) {
+    constructor(@InjectModel(User.name) private UserModel: UserModelType) {
     }
 
     async findById(id: string): Promise<UserDocument | null> {
         if (!Types.ObjectId.isValid(id)) return null;
         const searchItem: UserDocument | null
-            = await this.userModel.findOne({
+            = await this.UserModel.findOne({
                                             _id: new Types.ObjectId(id),
                                             deleteAt: null
                                         });
         return searchItem
+    }
+
+    async checkUniq(loginCheck: string, emailCheck: string):Promise<string[]|null>  {
+        // checks the uniqueness of the entered login and email, in case of duplication,
+        // returns an array indicating the duplicated field
+
+        const existEmail = await this.UserModel.countDocuments({ email: emailCheck })
+        const existLogin = await this.UserModel.countDocuments({ login: loginCheck })
+
+        if(existEmail > 0 || existLogin > 0) {
+            const arrayErrors: Array<string> = []
+            if(existEmail > 0)
+                arrayErrors.push('mail')
+            if(existLogin > 0)
+                arrayErrors.push('login')
+            return arrayErrors;
+        }
+        return null;
+    }
+
+    async findByConfirmCode(code: string): Promise<UserDocument|null> {
+        // search user with not verifed email
+
+        const searchItem: UserDocument | null
+            = await this.UserModel.findOne({
+            'confirmEmail.code': code,
+            isConfirmEmail: false})
+        return searchItem;
+    }
+
+    async getPartUserByLoginEmail(loginOrEmail: string): Promise<{id:string, passHash:string}|null> {
+
+        const checkedUser: UserDocument|null = await this.UserModel.findOne(
+            // returns information about the user who has
+            // a login or email that matches the passed value
+
+            {$and:[
+                    {isConfirmEmail: true},
+                    {$or: [
+                            {login: loginOrEmail},
+                            {email: loginOrEmail}]
+                    }]
+            })
+
+        return checkedUser === null
+            ? null
+            : {id: checkedUser._id.toString(), passHash: checkedUser.passwordHash};
+    }
+
+    async foundUserWithOutEmail(email: string):Promise <UserDocument|null>{
+        // search user with not verifed email
+
+        const searchItem: UserDocument | null
+            = await this.UserModel.findOne({email: email,
+            isConfirmEmail: false})
+
+        return searchItem;
+    }
+
+    async foundUserIdByEmail(mail: string):Promise <string|null>{
+        // search user with verifed email
+
+        const searchItem: UserDocument | null
+            = await this.UserModel.findOne({email: mail,
+            isConfirmEmail: true})
+        return searchItem ? searchItem._id.toString() : null;
     }
 
     async save(changedItem: UserDocument): Promise<void> {
