28772df0327b1529ecbf21111f3ba9759d514a58
Vladimir NG <potd3@ukr.net>
2025-05-19 19:05:25 +0300

    add guard session.is.active.ts

---
+++ src/core/guards/session.is.active.ts
@@ -0,0 +1,37 @@
+import { INJECT_TOKEN } from '@modules/users-system/constans/jwt.tokens';
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { SessionRepository } from '@modules/users-system/infrastucture/session.repository';
+import { DomainException } from '@core/exceptions/domain.exception';
+import { DomainExceptionCode } from '@core/exceptions/domain.exception.code';
+import { TokenPayloadDto } from '@modules/users-system/dto/tokenPayloadDto';
+
+@Injectable()
+export class SessionIsActiveGuard implements CanActivate {
+    constructor(@Inject(INJECT_TOKEN.REFRESH_TOKEN)
+                private readonly refreshJwtService: JwtService,
+                private readonly sessionRepository: SessionRepository,
+    ) {
+    }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token: string = request.cookies.refreshToken
+        try {
+            const payload: TokenPayloadDto = this.refreshJwtService.verify(token);
+            if (!await this.sessionRepository.isActive(payload))
+                throw new DomainException({
+                    message: 'refrashToken is not valid',
+                    code: DomainExceptionCode.Unauthorized
+                });
+
+            request.user = payload;
+            return true;
+        } catch {
+            throw new DomainException({
+                message: 'refrashToken is not valid',
+                code: DomainExceptionCode.Unauthorized
+            });
+        }
+    }
+}
