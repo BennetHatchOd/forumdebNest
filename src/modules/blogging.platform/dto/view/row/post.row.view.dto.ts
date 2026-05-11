9ae79041e21ab94b7deeb43eb7d25bd66eb1a895
Vladimir NG <potd3@ukr.net>
2026-05-12 02:58:15 +0300

    refactor(post):  add post.row.view.dto.ts

---
+++ src/modules/blogging.platform/dto/view/row/post.row.view.dto.ts
@@ -0,0 +1,18 @@
+import { Rating } from '../../enum/rating.enum';
+
+export class PostRowDto {
+    id: number;
+    title: string;
+    shortDescription: string;
+    content: string;
+    createdAt: string;
+    blogId: number;
+    blogName: string;
+    likesCount: number
+    dislikesCount: number;
+    myStatus: Rating;
+    newestLikes: {
+        addedAt: string,
+        userId: string,
+        login: string}[];
+}
