4950d4885ab0ef5bac2514aaf1f40cdddd2a4bf1
Vladimir NG <potd3@ukr.net>
2026-05-15 19:57:14 +0300

    refactor(like): add check for target existence

--- src/modules/blogging.platform/application/commands/make.like.usecase.ts
+++ src/modules/blogging.platform/application/commands/make.like.usecase.ts
@@ -2,6 +2,10 @@ import { Command, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { LikeCreateDto } from '@modules/blogging.platform/dto/create/like.create.dto';
 import { LikeRepository } from '@modules/blogging.platform/infrastucture/like.repository';
 import { Like } from '@modules/blogging.platform/domain/like.entity';
+import { DomainException } from '@core/exceptions/domain.exception';
+import { DomainExceptionCode } from '@core/exceptions/domain.exception.code';
+import { PostRepository } from '@modules/blogging.platform/infrastucture/post.repository';
+import { CommentRepository } from '@modules/blogging.platform/infrastucture/comment.repository';
 
 export class MakeLikeCommand extends Command<void> {
     constructor(public likeDto: LikeCreateDto) {
@@ -13,10 +17,27 @@ export class MakeLikeCommand extends Command<void> {
 export class MakeLikeHandler implements ICommandHandler<MakeLikeCommand> {
     constructor(
         private likeRepository: LikeRepository,
+        private postRepository: PostRepository,
+        private commentRepository: CommentRepository,
     ) {}
 
     async execute({ likeDto }: MakeLikeCommand): Promise<void> {
 
+        if(likeDto.targetType == 'post'){
+            const post = await this.postRepository.existsById(likeDto.targetId)
+            if(!post)
+                throw  new DomainException({
+                    message: 'post not found',
+                    code: DomainExceptionCode.NotFound,
+                });}
+        else {
+            const comment = await this.commentRepository.existsById(likeDto.targetId)
+            if(!comment)
+                throw  new DomainException({
+                    message: 'comment not found',
+                    code: DomainExceptionCode.NotFound,
+                });
+        }
         const foundLike: Like | null =
             await this.likeRepository.findLike(likeDto);
 
