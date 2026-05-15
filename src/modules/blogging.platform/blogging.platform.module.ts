dcd913c721b4d7740c5f0c4da77216cdb5348b93
Vladimir NG <potd3@ukr.net>
2026-05-15 04:33:48 +0300

    fix(blog): correct import for QueryHandlers

--- src/modules/blogging.platform/blogging.platform.module.ts
+++ src/modules/blogging.platform/blogging.platform.module.ts
@@ -9,7 +9,6 @@ import { CommentQueryRepository } from './infrastucture/query/comment.query.repo
 import { CommentRepository } from './infrastucture/comment.repository';
 import { PostController } from './api/post.controler';
 import { UserQueryExternalRepository } from '../users-system/infrastucture/query/user.query.external.repository';
-import { CommandHandlers } from 'src/modules/blogging.platform/application/commands';
 import { CqrsModule } from '@nestjs/cqrs';
 import { LikeRepository } from '@modules/blogging.platform/infrastucture/like.repository';
 import { AuthModule } from '@core/auth.module';
@@ -17,6 +16,7 @@ import { ReadUserIdGuard } from '@core/guards/read.userid';
 import { DatabaseModule } from '@core/database.module';
 import { BlogController } from '@modules/blogging.platform/api/blog.controller';
 import { QueryHandlers } from '@modules/blogging.platform/application/queries';
+import { CommandHandlers } from '@modules/blogging.platform/application/commands';
 
 @Module({
     imports: [
